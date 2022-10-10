7dc0ef92486ac32b2ecdc35ad674713595b5db9c
larissamagistrali <larissamagistrali@acad.pucrs.br>
2022-10-09 21:58:11 -0300

    Add personagem, cenario level 1 e movimentações

---
+++ gitbox-game/scripts/Player.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+var move_speed = 100
+var gravity = 1200
+var jump_force = -300
+
+func _physics_process(delta:float)->void:
+	velocity.y += gravity*delta
+	
+	_get_input()
+	
+	if Input.is_action_pressed("jump"):
+		velocity.y = jump_force
+		
+	velocity = move_and_slide(velocity)
+	
+func _get_input():
+	var move_direction = int(Input.is_action_pressed("move_right")) -  int(Input.is_action_pressed("move_left"))
+	velocity.x = move_speed*move_direction
+	if move_direction != 0:
+		$Sprite.scale.x = move_direction
