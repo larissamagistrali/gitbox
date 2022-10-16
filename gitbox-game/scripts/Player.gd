e4f73708de23d5c3be73198dcfcae753c527cbc2
larissamagistrali <larissamagistrali@acad.pucrs.br>
2022-10-15 22:34:49 -0300

    Add mecanica empurrar caixa

--- gitbox-game/scripts/Player.gd
+++ gitbox-game/scripts/Player.gd
@@ -10,9 +10,16 @@ onready var raycasts = $"Raycasts-Node2D"
 func _physics_process(delta:float)->void:
 	velocity.y += gravity*delta
 	_get_input()
+	#mover caixa
+	if $"Push-Node2D/RayCast2D-right".is_colliding():
+		var obj = $"Push-Node2D/RayCast2D-right".get_collider()
+		obj.move_and_slide(Vector2(30,0)*move_speed*delta)
+	elif $"Push-Node2D/RayCast2D-left".is_colliding():
+		var obj = $"Push-Node2D/RayCast2D-left".get_collider()
+		obj.move_and_slide(Vector2(-30,0)*move_speed*delta)
 	velocity = move_and_slide(velocity)
 	is_grounded = _check_is_grounded()
-	_set_animation()
+	_set_animation()	
 	
 func _get_input():
 	velocity.x=0
@@ -20,7 +27,16 @@ func _get_input():
 	velocity.x = lerp(velocity.x, move_speed*move_direction,0.2)
 	if move_direction != 0:
 		$Sprite.scale.x = move_direction
-
+	#mover caixa
+	if velocity.x>0:
+		$"Push-Node2D/RayCast2D-right".set_enabled(true)
+	else:
+		$"Push-Node2D/RayCast2D-right".set_enabled(false)
+	if velocity.x<0:
+		$"Push-Node2D/RayCast2D-left".set_enabled(true)
+	else:
+		$"Push-Node2D/RayCast2D-left".set_enabled(false)
+	
 func _input(event: InputEvent)->void:
 	if Input.is_action_pressed("jump") and is_grounded:
 		velocity.y = jump_force
