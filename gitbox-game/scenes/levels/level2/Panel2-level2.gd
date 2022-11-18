6d08180900c8cc744b1d647e31fdb7eb19807b6c
larissamagistrali <larissamagistrali@acad.pucrs.br>
2022-11-18 16:12:00 -0300

    Inicio fase 2

---
+++ gitbox-game/scenes/levels/level2/Panel2-level2.gd
@@ -0,0 +1,22 @@
+extends Panel
+
+onready var current_scene = get_parent().get_node("Panel").get_parent().get_node(".")
+onready var changer = get_parent().get_node("Transition_in")
+
+export var path : String
+
+func _ready():
+	pass
+	
+func _on_btnScene1_pressed():
+	current_scene.saveState()
+	changer.change_scene(path)
+
+func _on_btnResetScene2_pressed():
+	current_scene.reset()
+	
+func _on_btnMergeScene2_pressed():
+	current_scene.merge(Level1Global.objects1)
+
+func _on_btnMenu_pressed() -> void:
+	changer.change_scene("res://scenes/itens/menu.tscn")
